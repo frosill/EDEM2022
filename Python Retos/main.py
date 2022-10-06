f08df1541f1b5b90691ef1b456873719f04b32e8
Fran <fran.rosillo.10@gmail.com>
2022-10-06 11:23:49 +0200

    Se añaden Retos Python

---
+++ Python Retos/main.py
@@ -0,0 +1,17 @@
+import datetime
+from Retos.Reto14 import volumen_cilindro
+from Retos.Reto15 import cuadrados
+from Retos.Reto16 import resta_dias
+from Retos.Reto18 import eliminar
+from Retos.Reto1_avanzado import venta_discos
+
+print(f'El volumen de tu cilindro es de [{volumen_cilindro(2,4)}m^3')   
+
+
+
+
+
+
+
+
+
