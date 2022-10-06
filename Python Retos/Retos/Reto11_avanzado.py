9f3876048bd07d6637269c655d563ddcaf2c7f42
Fran <fran.rosillo.10@gmail.com>
2022-10-06 11:43:46 +0200

    Create Reto11_avanzado.py

---
+++ Python Retos/Retos/Reto11_avanzado.py
@@ -0,0 +1,15 @@
+class cliente():
+
+    #Inicializamos atributos
+    def __init__(self, NIF : str, nombre : str, apellidos : str, telefono: str, email : str):
+
+        self.NIF = NIF
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.telefono = telefono
+        self.email = email
+        self.preferente = False
+
+    
+
+
